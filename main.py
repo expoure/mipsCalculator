f1cb44f69fa2e3596bcc8592c4f4bd439fd2567b
dkramer <dkramer97@hotmail.com>
2018-05-25 19:42:28 -0300

    :D

--- main.py
+++ main.py
@@ -7,14 +7,25 @@ print ("Instruções:")
 print ("A calculadora realiza SOMA, SUBTRAÇÃO, MULTIPLICAÇÃO, DIVISÃO, RAIZ QUADRADA, POTENCIA, FATORIAL E FIBONACCI de "
        "operadores do tipo INTEIRO")
 
+dataArray = []
+savedTemporary = ["$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7"]
+temporary = ["$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7"]
 
-
-
-def dataWrite(): #essa funcao ira escrever na parte da estrutura referente aos operandos. .data:
+def dataWrite(): #essa funcao ira escrever no arquivo
     file = open("calc.asm", "a+")
-    file.write(".data\n")
-    for i in range(0, len(operands[0])):
-        file.write("  " + az[0][i] + ": .word " + str(operands[0][i]) + "\n") #pega a letra do alfabeto de acordo com a necessidade e refere ao operando
+    file.write("\n.data\n")
+    for i in range(0, len(operands)):
+        file.write("\t" + az[0][i] + ": .word " + str(operands[i]) + "\n") #pega a letra do alfabeto de acordo com a necessidade e refere ao operando
+        dataArray.append(az[0][i])
+    file.write("\n.text\n\tmain:\n") #adiciona a parte .text e main:
+
+    for i in range(0, len(operands)):
+        file.write("\t\tlw " + str(savedTemporary[i]) + ", " + str(dataArray.pop(0)) + "\n") #faz load word e atribui o dado a um registrador
+
+    #preciso arrumar a estrutura para que as operacoes sejam feitas de forma correta
+    for i in range(0, len(operators)):
+        if operators[i] == "+":
+            file.write("\t\tadd " + str(temporary[i]) + ", " + str(savedTemporary.pop(0) + ", " + str(savedTemporary.pop(0))) + "\n")
     file.close()
 
 
@@ -24,7 +35,7 @@ file.close()
 
 operators = [] #lista dinamica dos operadores
 operators_index = [] #lista dinamica do indice dos operadores
-operands = [] #lista dinamica dos operandos
+operandsFromString = [] #lista dinamica dos operandos
 az = ["abcdefghijklmnopqrstuvwxyz"]
 
 expression = input("Entre com o calculo a ser realizado: ")
@@ -32,13 +43,6 @@ for i in range(0, len(expression)): #aqui ele 'escaneia' a string em busca de op
     if not ((expression[i].isnumeric()) or (expression[i].isspace())):
         operators.append(expression[i])  #adiciona operadores na lista
         operators_index.append(i)   #adiciona o indice dos operadores na lista
-operands.append(re.findall('\d+', expression)) #faz uma lista com
+operandsFromString.append(re.findall('\d+', expression)) #faz uma lista com operandos
+operands = operandsFromString.pop() #pega sub vetor e passa para vetor normal
 dataWrite()
-
-
-
-print (len(operands[0]))
-print (az)
-print (operators)
-print (operators_index)
-print (operands[0][1])
