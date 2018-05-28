8e5b5672330a89ebd74cad0f0bd2add6c98219a5
dkramer <dkramer97@hotmail.com>
2018-05-27 21:42:52 -0300

    :D

--- calc.asm
+++ calc.asm
@@ -1,6 +1,2 @@
 #Trabalho de arquitetura de computadores 1 - Dyonatha Kramer e Laerte Pack
 #Calculadora MIPS
-.data
-  a: .word 25
-  b: .word 4
-  c: .word 2
--- main.py
+++ main.py
@@ -7,6 +7,7 @@ print ("Instruções:")
 print ("A calculadora realiza SOMA, SUBTRAÇÃO, MULTIPLICAÇÃO, DIVISÃO, RAIZ QUADRADA, POTENCIA, FATORIAL E FIBONACCI de "
        "operadores do tipo INTEIRO")
 
+priority = []
 dataArray = []
 savedTemporary = ["$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7"]
 temporary = ["$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7"]
@@ -23,8 +24,19 @@ def dataWrite():     #essa funcao ira escrever no arquivo
         file.write("\t\tlw " + str(savedTemporary[i]) + ", " + str(dataArray.pop(0)) + "\n")    #faz load word e atribui o dado a um registrador
 
     #preciso arrumar a estrutura para que as operacoes sejam feitas de forma correta
-    for i in range(0, len(operators)):
+
+        if operators[i] == "*":
+            file.write("\t\tmult " + str(temporary[i]) + ", " + str(savedTemporary.pop(0) + ", " + str(savedTemporary.pop(0))) + "\n")
+
+        if operators[i] == "/":
+            file.write("\t\tdiv " + str(temporary[i]) + ", " + str(savedTemporary.pop(0) + ", " + str(savedTemporary.pop(0))) + "\n")
+
+
+    for i in range(0, len(operators) -1):
         if operators[i] == "+":
+            file.write("\t\tadd " + str(temporary[i]) + ", " + str(savedTemporary.pop(0) + ", " + str(savedTemporary.pop(0))) + "\n")
+
+        if operators[i] == "-":
             file.write("\t\tadd " + str(temporary[i]) + ", " + str(savedTemporary.pop(0) + ", " + str(savedTemporary.pop(0))) + "\n")
     file.close()
 
@@ -39,10 +51,17 @@ operandsFromString = []     #lista dinamica dos operandos
 az = ["abcdefghijklmnopqrstuvwxyz"]
 
 expression = input("Entre com o calculo a ser realizado: ")
+
+
 for i in range(0, len(expression)):     #aqui ele 'escaneia' a string em busca de operadores
     if not ((expression[i].isnumeric()) or (expression[i].isspace())):
         operators.append(expression[i])     #adiciona operadores na lista
         operators_index.append(i)       #adiciona o indice dos operadores na lista
 operandsFromString.append(re.findall('\d+', expression))     #faz uma lista com operandos
 operands = operandsFromString.pop()     #pega sub vetor e passa para vetor normal
-dataWrite()
+
+#agora que tenho prioridade de expressao devo implementar codigo para que faca operacoes de forma correta em assembly
+#para mais de duas operacoes. Tambem preciso implementar em assembly as expressoes fora dos parenteses.
+priority = (re.findall(r'\((.*?)\)', expression))
+print(priority)
+
