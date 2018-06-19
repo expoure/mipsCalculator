9cc87ecec194e66f63bac871b104191b3dc29445
dkramer <dkramer97@hotmail.com>
2018-06-19 18:28:42 -0300

    :D

--- main.py
+++ main.py
@@ -2,14 +2,12 @@ import re
 import shuntingYardAlgorithm
 
 
-
 presentation = "Trabalho de arquitetura de computadores 1 - Dyonatha Kramer e Laerte Pack\n#Calculadora MIPS\n"
 print ("\n" + presentation)
 print ("Instruções:")
 print ("A calculadora realiza SOMA, SUBTRAÇÃO, MULTIPLICAÇÃO, DIVISÃO, POTENCIA e FATORIAL de "
        "operadores do tipo INTEIRO")
 
-sizeArray = 0
 operandsInOrder = []
 dataArray = []
 savedTemporary = ["$s0", "$s1", "$s2", "$s3", "$s4", "$s5", "$s6", "$s7"]
@@ -18,23 +16,19 @@ temporary = ["$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7"]
 temporaryUsed = []
 
 def dataWrite():     #essa funcao ira escrever no arquivo
-    poped = 0
+    poped = 0 #identa toda vez que um elemento eh retirado do rpn para que seja possivel fazer manipulacao de vetores corretamente
     count = 0
     file = open("calc.asm", "a+")
     file.write("\n.data\n")
-    for i in range(0, len(operands)):
+    for i in range(0, len(operands)): #vai carregar o word de acordo com os numeros do vetor ordenado
         file.write("\t" + az[0][i] + ": .word " + str(operandsInOrder[i]) + "\n")  #pega a letra do alfabeto de acordo com a necessidade e refere ao operando
         dataArray.append(az[0][i])
     file.write("\n.text\n\tmain:\n")    #adiciona a parte .text e main:
 
-    #for i in range(0, len(operands)):
-    #    file.write("\t\tlw " + str(savedTemporary[i]) + ", " + str(dataArray.pop(0)) + "\n")    #faz load word e atribui o dado a um registrador
-
-
-    for i in range(0, len(sya)):
+    for i in range(0, len(sya)): #vai varrer a lista padrao
         if not sya[i].isnumeric():
-            #if sya[i] == "r":
 
+            #trecho referente ao fatorial
             if sya[i] == "f":
                 if len(rpn) != 0:
                     if rpn[0].isnumeric():
@@ -44,7 +38,7 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                                    "\nLOOP:\n\t\taddi $t4, $t4, 1\n\t\t\tslt $t0, $t4, $s4\n\t\t\tbeq $t0, $zero, LSair\n\t\tmul $t2, $t2, $t4"
                                    "\n\t\tj LOOP\nLSair:\n\t\tsw $t2, ($s3)\n\t\tli $v0, 1\n\t\tmove $a0, $t2\n\t\tsyscall")
                         exit()
-
+            #trecho referente a potencia de x e y
             if sya[i] == "^":
                 if len(rpn) != 0:
                     if rpn[i-1].isnumeric() and rpn[i-2].isnumeric():
@@ -52,11 +46,11 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                         file.write("\n\t\taddi $t0,$s0, 0\n\t\taddi $t1,$zero, 0\n\twhile:\n\t\tblt $s1, 2, exit\n\t\tsubi $s1, $s1, 1"
                                    "\n\t\tmul $t0, $t0, $s0\n\t\tj while\n\texit:\n\t\tli $v0, 1\n\t\tmove $a0, $t0\n\t\tsyscall")
                         exit()
-
+            #trecho referente a multiplicacao
             if sya[i] == "*":
                 print(str(rpn) + '*')
                 if len(rpn) != 0:
-                    if rpn[i-2 -poped].isnumeric() and rpn[i-1 - poped].isnumeric():
+                    if rpn[i - 2 - poped].isnumeric() and rpn[i - 1 - poped].isnumeric(): #verifica argumentos de rpn com base em sya utilizando 'poped'
                         file.write("\t\tlw " + str(savedTemporary[0]) + ", " + str(dataArray.pop(i-2-poped)) + "\n")
                         file.write("\t\tlw " + str(savedTemporary[1]) + ", " + str(dataArray.pop(i-2-poped)) + "\n")
                         temporaryUsed.append(temporary.pop(0))
@@ -64,27 +58,24 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                             savedTemporary[1]) + "\n")
                         count += 1
                         try:
-                            for j in range(i-poped, i-poped-3, -1):
+                            for j in range(i-poped, i-poped-3, -1):     #elimina operacoes e operandos ja utilizados
                                 rpn.pop(j)
                                 poped += 1
                                 print(str(rpn) + '*')
                         except Exception:
                             print('..')
 
-
-
                     else:
                         print(i)
                         file.write("\t\tlw " + str(savedTemporary[0]) + ", " + str(dataArray.pop(0)) + "\n")
                         file.write("\t\tmul " + str(temporaryUsed[0]) + ", " + str(temporaryUsed[0]) + ", " + str(
                             savedTemporary[0]) + "\n")
-                        sizeArray = len(rpn)
-                        for j in range(i - poped, i - poped - 2, -1):  #for j in range(i, rpn[0], -1):
+                        for j in range(i - poped, i - poped - 2, -1):  #elimina operacoes e operandos ja utilizados
                             rpn.pop(0)
                             poped += 1
                             print(str(rpn) + '*')
 
-
+            # trecho referente a divisao
             if sya[i] == "/":
                 print(str(rpn) + '/')
                 if len(rpn) != 0:
@@ -97,26 +88,24 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                             savedTemporary[1]) + "\n")
                         count += 1
                         try:
-                            print(i-poped)
-                            for j in range(i-poped, i-poped-3, -1):
+                            for j in range(i-poped, i-poped-3, -1): #elimina operacoes e operandos ja utilizados
                                 rpn.pop(j)
                                 poped += 1
                                 print(str(rpn) + '/')
                         except Exception:
                             print('..')
-                        print('saiu')
 
                     else:
                         print(i)
                         file.write("\t\tlw " + str(savedTemporary[0]) + ", " + str(dataArray.pop(0)) + "\n")
                         file.write("\t\tdiv " + str(temporaryUsed[0]) + ", " + str(temporaryUsed[0]) + ", " + str(
                             savedTemporary[0]) + "\n")
-                        sizeArray = len(rpn)
-                        for j in range(i - poped, i - poped - 2, -1):  #for j in range(i, rpn[0], -1):
+                        for j in range(i - poped, i - poped - 2, -1):  #elimina operacoes e operandos ja utilizados
                             rpn.pop(0)
                             poped += 1
                             print(str(rpn) + '/')
 
+            # trecho referente a adicao
             if sya[i] == "+":
                 print(str(rpn) + '+')
                 if len(rpn) > 1:
@@ -128,7 +117,7 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                             savedTemporary[1]) + "\n")
                         count += 1
                         try:
-                            for j in range(i-poped, i-poped-3, -1):
+                            for j in range(i-poped, i-poped-3, -1):  #elimina operacoes e operandos ja utilizados
                                 rpn.pop(j)
                                 poped += 1
                                 print(str(rpn) + '+')
@@ -140,14 +129,14 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                         file.write("\t\tlw " + str(savedTemporary[0]) + ", " + str(dataArray.pop(0)) + "\n")
                         file.write("\t\tadd " + str(temporaryUsed[0]) + ", " + str(temporaryUsed[0]) + ", " + str(
                             savedTemporary[0]) + "\n")
-                        sizeArray = len(rpn)
-                        for j in range(i-poped, i-poped-2, -1):      #for j in range(i, rpn[0], -1):
+                        for j in range(i-poped, i-poped-2, -1): #elimina operacoes e operandos ja utilizados
                             rpn.pop(0)
                             poped += 1
                 if len(temporaryUsed) > 1:
                     file.write("\t\tadd " + str(temporaryUsed[0]) + ", " + str(temporaryUsed.pop(0)) + ", " + str(
                         temporaryUsed.pop(0)) + "\n")
 
+            # trecho referente a subtracao
             if sya[i] == "-":
                 print(str(rpn) + '-')
                 if len(rpn) > 1:
@@ -159,7 +148,7 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                             savedTemporary[1]) + "\n")
                         count += 1
                         try:
-                            for j in range(i-poped, i-poped-3, -1):
+                            for j in range(i-poped, i-poped-3, -1):  #elimina operacoes e operandos ja utilizados
                                 rpn.pop(j)
                                 poped += 1
                                 print(str(rpn) + '-')
@@ -171,8 +160,7 @@ def dataWrite():     #essa funcao ira escrever no arquivo
                         file.write("\t\tlw " + str(savedTemporary[0]) + ", " + str(dataArray.pop(0)) + "\n")
                         file.write("\t\tsub " + str(temporaryUsed[0]) + ", " + str(temporaryUsed[0]) + ", " + str(
                             savedTemporary[0]) + "\n")
-                        sizeArray = len(rpn)
-                        for j in range(i - poped, i - poped - 2, -1):  #for j in range(i, rpn[0], -1):
+                        for j in range(i - poped, i - poped - 2, -1):  #elimina operacoes e operandos ja utilizados
                             print(str(rpn) + '-')
                             rpn.pop(0)
                             poped += 1
@@ -183,6 +171,8 @@ def dataWrite():     #essa funcao ira escrever no arquivo
 
     file.write("\t\tli $v0, 1\n\t\tmove $a0, $t0\n\t\tsyscall")
     file.close()
+
+
 
 
 file = open("calc.asm", "w+")
@@ -213,6 +203,6 @@ for i in range(0, len(sya)):
 
 for i in range(0, len(rpn)):
     if rpn[i].isnumeric():
-        operandsInOrder.append(rpn[i])
+        operandsInOrder.append(rpn[i]) #coloca os operadores em outra lista e em ordem para serem usados no data
 print(rpn)
 dataWrite()
